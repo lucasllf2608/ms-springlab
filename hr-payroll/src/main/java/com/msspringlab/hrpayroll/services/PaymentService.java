3ff754e0201be0af571fe4dae2497673bf020023
lucasllf2608 <lucas.llf2608@gmail.com>
2025-07-10 07:49:48 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/msspringlab/hrpayroll/services/PaymentService.java
@@ -0,0 +1,15 @@
+package com.msspringlab.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.msspringlab.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	
+	public Payment getPayment(long workedId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+	
+}
