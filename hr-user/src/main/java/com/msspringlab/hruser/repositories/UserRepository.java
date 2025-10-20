382920113af08f26fef6e285193aff48e6ed3979
lucasllf2608 <lucas.llf2608@gmail.com>
2025-10-20 20:38:55 -0300

    changing the hr-user project to test endpoints

---
+++ hr-user/src/main/java/com/msspringlab/hruser/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.msspringlab.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.msspringlab.hruser.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+  
+	User findByEmail(String email);
+	
+	
+}
---
+++ hr-user/src/main/java/com/msspringlab/hruser/resources/UserResources.java
@@ -0,0 +1,47 @@
+package com.msspringlab.hruser.resources;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.msspringlab.hruser.entities.User;
+import com.msspringlab.hruser.repositories.UserRepository;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+	@Autowired
+	private UserRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = repository.findAll();
+
+		return ResponseEntity.ok(list);
+	} 
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long  id){
+		User obj = repository.findById(id).get();
+		return ResponseEntity.ok(obj);
+	} 
+	
+	
+	@GetMapping(value = "/teste")
+	public void teste(){
+			System.out.println("te4ste");
+	} 
+	
+	
+}
